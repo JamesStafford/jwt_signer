50649cb474daa4657b0f8146ffe01ffeddb67cfd
jstafford <CatMouse2017!>
2021-08-13 23:59:43 -0500

    Removed the code around user input and output (apparently jest doesn't support this well)
    Hardcoded the path for data files for simplicity sake.
    
    Created main for execution purposes

--- src/signer.js
+++ src/signer.js
@@ -1,33 +1,51 @@
 import jwt from "jsonwebtoken";
-const readline = require("readline");
+import fs from "fs";
 
-export const MissingPayloadAndPrivateKeyError = "Payload and Private Key are required to sign a JWT";
+export const MissingPayloadAndPrivateKeyError =
+  "Payload and Private Key are required to sign a JWT";
 export const MissingPayloadError = "Payload is required to sign a JWT";
 export const MissingPrivateKeyError = "Private Key is required to sign a JWT";
 
 export function sign(payload, privateKey) {
-    if (!payload && !privateKey) throw new Error(MissingPayloadAndPrivateKeyError);
-    if (!payload) throw new Error(MissingPayloadError);
-    if (!privateKey) throw new Error(MissingPrivateKeyError);
-    return jwt.sign(payload, privateKey);
+  if (!payload && !privateKey) {
+    throw new Error(MissingPayloadAndPrivateKeyError);
+  }
+
+  if (!payload) {
+    throw new Error(MissingPayloadError);
+  }
+
+  if (!privateKey) {
+    throw new Error(MissingPrivateKeyError);
+  }
+
+  return jwt.sign(payload, privateKey);
 }
 
-export const PromptForPayloadFilePath = "Please enter filepath to payload: ";
+export function main() {
+  const fileSystem = fs;
 
-export function processUserInput() {
-    const readLineInterface = readline.createInterface({
-       input: process.stdin,
-       output: process.stdout
-    });
+  const pathToPayload = "data/payload.json";
+  let payload = null;
+  try {
+    payload = fileSystem.readFileSync(pathToPayload);
+  } catch(error) {
+    throw `Failed to read data from payloadPath: "${pathToPayload}" with error: "${error.toString()}"`
+  }
 
-    readLineInterface.write(PromptForPayloadFilePath);
+  const pathToPrivateKey = "data/privateKey.rem";
+  let privateKey = null;
+  try {
+    privateKey = fileSystem.readFileSync(pathToPrivateKey);
+  } catch(error) {
+    throw `Failed to read data from pathToPrivateKey: "${pathToPrivateKey}" with error: "${error.toString()}"`
+  }
 
-    // readLineInterface.on("Hello, world", () => {});
-
-    return {
-        keyPlaceholder: "",
-        payloadPlaceholder: {}
-    }
+  const signedJWT = sign(payload, privateKey);
+  const pathToSignedToken = "data/signedToken.txt";
+  try {
+    fileSystem.writeFileSync(pathToSignedToken, signedJWT);
+  } catch(error) {
+      throw `Failed to write key to pathToSignedToken: "${pathToSignedToken}" with error: "${error.toString()}"`
+  }
 }
-
-processUserInput();
--- tests/signer.test.js
+++ tests/signer.test.js
@@ -1,12 +1,9 @@
-import * as mockProcess from "jest-mock-process";
-
 import {
-    MissingPayloadAndPrivateKeyError,
-    MissingPayloadError,
-    MissingPrivateKeyError, processUserInput, PromptForPayloadFilePath,
-    sign,
+  MissingPayloadAndPrivateKeyError,
+  MissingPayloadError,
+  MissingPrivateKeyError,
+  sign,
 } from "../src/signer";
-
 
 jest.mock("jsonwebtoken", () => ({
   sign: () => "",
@@ -17,53 +14,34 @@ const keyPlaceholder = "key placeholder";
 
 describe("For Signer, ", () => {
   describe("Testing Signing, ", () => {
-    test("Signer returns a value when provided payload and key.", () => {
+    test("Verify signer returns a value when provided payload and key.", () => {
       expect(sign(payloadPlaceholder, keyPlaceholder)).toBe("");
     });
 
-    test("Signer throws an error no parameters are not provided", () => {
+    test("Verify signer throws an error no parameters are not provided", () => {
       expect(() => sign()).toThrow(MissingPayloadAndPrivateKeyError);
     });
 
-    test("Signer throws an error when payload is falsey", () => {
+    test("Verify signer throws an error when payload is falsey", () => {
       const emptyPayloadPlaceholder = null;
       expect(() => sign(emptyPayloadPlaceholder, keyPlaceholder)).toThrow(
         MissingPayloadError
       );
     });
 
-    test("Signer throws an error when key is not provided", () => {
+    test("Verify signer throws an error when key is not provided", () => {
       const emptyKeyPlaceholder = "";
       expect(() => sign(payloadPlaceholder, emptyKeyPlaceholder)).toThrow(
         MissingPrivateKeyError
       );
     });
 
-    test("Signer throws an error when payload and key are not falsey", () => {
+    test("Verify signer throws an error when payload and key are not falsey", () => {
       const emptyPayloadPlaceholder = null;
       const emptyKeyPlaceholder = "";
       expect(() => sign(emptyPayloadPlaceholder, emptyKeyPlaceholder)).toThrow(
         MissingPayloadAndPrivateKeyError
       );
-    });
-  });
-
-  describe("Testing Handling User Input, ", () => {
-    test("Verify that processUserInput method exists.", () => {
-        expect(() => processUserInput()).not.toThrow();
-    });
-
-    test("Receive object from processUserInput.", () => {
-        expect(processUserInput()).toStrictEqual({
-            payloadPlaceholder: {},
-            keyPlaceholder: ""
-        });
-    });
-
-    test("User is requested to enter filepath for payload.", () => {
-        const mockedStdOut = mockProcess.mockProcessStdout();
-        processUserInput();
-        expect(mockedStdOut).toHaveBeenCalledWith(PromptForPayloadFilePath);
     });
   });
 });
