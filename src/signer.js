3ca12979576b390cbff03dd9a5c81b11397df5a2
jstafford <CatMouse2017!>
2021-08-13 22:58:44 -0500

    Added tests and logic for sign's parameters Mocked jsonwebtoken and return "" for sign's value in signer.test.js

--- src/signer.js
+++ src/signer.js
@@ -1,4 +1,4 @@
-// const jwt = require("jsonwebtoken");
+import jwt from "jsonwebtoken";
 // const readline = require("readline");
 
 // const payload = {};
@@ -6,4 +6,13 @@
 //
 // jwt.sign(payload, privateKey);
 
-export function sign() {}
+export const MissingPayloadAndPrivateKeyError = "Payload and Private Key are required to sign a JWT";
+export const MissingPayloadError = "Payload is required to sign a JWT";
+export const MissingPrivateKeyError = "Private Key is required to sign a JWT";
+
+export function sign(payload, privateKey) {
+    if (!payload && !privateKey) throw new Error(MissingPayloadAndPrivateKeyError);
+    if (!payload) throw new Error(MissingPayloadError);
+    if (!privateKey) throw new Error(MissingPrivateKeyError);
+    return jwt.sign(payload, privateKey);
+}
--- tests/signer.test.js
+++ tests/signer.test.js
@@ -1,7 +1,45 @@
-import {sign} from "../src/signer"
+import {
+  MissingPayloadAndPrivateKeyError,
+  MissingPayloadError,
+  MissingPrivateKeyError,
+  sign,
+} from "../src/signer";
+
+jest.mock("jsonwebtoken", () => ({
+  sign: () => "",
+}));
+
+const payloadPlaceholder = { isPlaceholder: true };
+const keyPlaceholder = "key placeholder";
 
 describe("For Signer, ", () => {
-    test("Signer returns a value when provided payload and key.", () => {
-        sign();
-    })
+  test("Signer returns a value when provided payload and key.", () => {
+    expect(sign(payloadPlaceholder, keyPlaceholder)).toBe("");
+  });
+
+  test("Signer throws an error no parameters are not provided", () => {
+    expect(() => sign()).toThrow(MissingPayloadAndPrivateKeyError);
+  });
+
+  test("Signer throws an error when payload is falsey", () => {
+    const emptyPayloadPlaceholder = null;
+    expect(() => sign(emptyPayloadPlaceholder, keyPlaceholder)).toThrow(
+      MissingPayloadError
+    );
+  });
+
+  test("Signer throws an error when key is not provided", () => {
+    const emptyKeyPlaceholder = "";
+    expect(() => sign(payloadPlaceholder, emptyKeyPlaceholder)).toThrow(
+      MissingPrivateKeyError
+    );
+  });
+
+  test("Signer throws an error when payload and key are not falsey", () => {
+    const emptyPayloadPlaceholder = null;
+    const emptyKeyPlaceholder = "";
+    expect(() => sign(emptyPayloadPlaceholder, emptyKeyPlaceholder)).toThrow(
+      MissingPayloadAndPrivateKeyError
+    );
+  });
 });
